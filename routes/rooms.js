e4cae508f35baeebe87717f0580896037779c09f
ysfoz <ysfozturk3@gmail.com>
2022-06-11 12:50:29 +0200

    added room model and methods

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,59 @@
 import express from "express";
 
+import {
+createRoom,deleteRoom,updateRoom,getRoom,getRooms
+} from "../controllers/room.js";
+import { verifyToken, verifyAdmin } from "../utils/verifyToken.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("3");
-});
+/**
+ * !CREATE
+ * @method post
+ * @route api/room:hotelid
+ * @description
+ * @access private
+ */
+
+router.post("/:hotelid",verifyToken, verifyAdmin, createRoom);
+
+/**
+ * !UPDATE
+ * @method put
+ * @route api/room:id
+ * @description
+ * @access private
+ */
+
+router.put("/:id", verifyToken, verifyAdmin, updateRoom);
+
+/**
+ * !DELETE
+ * @method delete
+ * @route api/room:id
+ * @description
+ * @access private
+ */
+
+router.delete("/:id/:hotelid", verifyToken, verifyAdmin, deleteRoom);
+
+/**
+ * !GET ONE
+ * @method get
+ * @route api/room:id
+ * @description
+ * @access public
+ */
+router.get("/:id", getRoom);
+
+/**
+ * !GET ALL
+ * @method get
+ * @route /api/room
+ * @description
+ * @access public
+ */
+
+router.get("/", getRooms);
 
 export default router;
